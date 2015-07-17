77cfabe8b7b52396474bfcd25179e44bd11f6e91
Cezar Rosa <cecez86@gmail.com>
2015-07-17 19:43:00 -0300

    teste para consulta de uma mensagem

--- exemplos/testeMensagens.php
+++ exemplos/testeMensagens.php
@@ -3,25 +3,25 @@
 include_once '../Criaenvio_loader.php';
 
 // VOCÊ DEVE ALTERAR SUA CHAVE AQUI
-define('NN_CHAVE', '=sua chave=');
+define('NN_CHAVE', '=chave=');
 
-try {
-
-    $parametros = null; // sem parâmetros
- //   $parametros = array('id'=>'-W, -y');      // filtrando pelo id
- //   $parametros = array('assunto' => 'as');     // pelo assunto
- //   $parametros = array('contador' => '93');     // pelo contador
- //   $parametros = array('datadecadastroinicio' => '2015-03-05', 'datadecadastrotermino' => '2019-03-05');     // pela data
-   // $parametros = array('nome' => 'tesste','id' => '-Q');  // nome e id
-
-    $mensagens = (new Criaenvio\Mensagem())->buscar($parametros);
-
-    echo '<pre>';
-    print_r($mensagens->getDados());
-
-} catch (Exception $e) {
-    var_dump($e); die;
-}
+//try {
+//
+//    $parametros = null; // sem parâmetros
+// //   $parametros = array('id'=>'-W, -y');      // filtrando pelo id
+// //   $parametros = array('assunto' => 'as');     // pelo assunto
+// //   $parametros = array('contador' => '93');     // pelo contador
+// //   $parametros = array('datadecadastroinicio' => '2015-03-05', 'datadecadastrotermino' => '2019-03-05');     // pela data
+//   // $parametros = array('nome' => 'tesste','id' => '-Q');  // nome e id
+//
+//    $mensagens = (new Criaenvio\Mensagem())->buscar($parametros);
+//
+//    echo '<pre>';
+//    print_r($mensagens->getDados());
+//
+//} catch (Exception $e) {
+//    var_dump($e); die;
+//}
 
 /**
  * Removendo uma mensagem.
@@ -38,6 +38,20 @@ try {
 //        echo 'falha';
 //    }
 //
+//} catch (Exception $e) {
+//    echo $e->getMessage();
+//}
+
+/**
+ * Carregando uma mensagem.
+ */
+//try {
+//    $id = '-y';
+////    $id = 'n0Ex1st3';
+//
+//    $campo = (new Criaenvio\Mensagem($id))->carregar();
+//    echo '<pre>';
+//    print_r($campo);
 //} catch (Exception $e) {
 //    echo $e->getMessage();
 //}
